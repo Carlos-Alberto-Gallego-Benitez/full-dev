85cf3c55668c8ea079064200ebd2a47d50a17027
Armando <gomezuruetaarmandojose@gmail.com>
2023-03-04 12:04:18 -0500

    ajustes al diseño

--- script.js
+++ script.js
@@ -34,12 +34,12 @@ $(document).ready(function(){
     });
 
     // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing", {
+    //     strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 
     var typed = new Typed(".typing-2", {
         strings: ["Java", "Python", "C++", "Node js", "React", "Angular", "Js", "PHP", "C#/.NET"],
